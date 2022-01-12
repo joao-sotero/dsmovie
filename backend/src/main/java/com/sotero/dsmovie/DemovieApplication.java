304397a76e847bb0cb44772eed2e7f0d4621ac34
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2022-01-12 09:36:27 -0300

    project crated

---
+++ backend/src/main/java/com/sotero/dsmovie/DemovieApplication.java
@@ -0,0 +1,13 @@
+package com.sotero.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sotero/dsmovie/DemovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotero.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
